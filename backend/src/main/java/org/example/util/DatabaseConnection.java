bba7e8b6e1f11eb7c3eae551fc016d6f2c870291
samad27 <samadshaikh2702@gmail.com>
2025-02-21 10:49:56 +0530

    sql queries written in schema

--- backend/src/main/java/org/example/util/DatabaseConnection.java
+++ backend/src/main/java/org/example/util/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/loan_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root_1234";
+    private static final String PASSWORD = "Samad@2702";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
